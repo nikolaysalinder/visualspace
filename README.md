5dda5778043ff01fac165a6be898817b30f2027b
nikolaysalinder <nikolay.salinder@gmail.com>
2017-02-07 09:07:34 +0300

    delete big files, add 3 cubes

---
+++ README.md
@@ -0,0 +1,9 @@
+# VisualSpace 
+Choose your future.
+
+We creating Virtual Reality marketplace for real estate,
+where you can buy or rent your future home just using one click.
+If you want to become a part of our team,
+fill free to contact: nikolay.salinder@gmail.com.
+'Visual Space' All rights reserved.
+
---
+++ server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+//Подключаем папку public для статических файлов
+app.use(express.static(__dirname + '/public'));
+
+//Подключаем шаблонизатор handlebars изпользую шаблон main в папке views/layouts
+var handlebars = require('express-handlebars').create({
+  defaultLayout:'main',
+  //этот код надо внести для того чтобы не было Error: Missing helper "section"
+  //this piece of a code is necessary to avoid Error: Missing helper "section"
+  helpers: {
+    section: function(name, options){
+      if(!this._sections) this._sections = {};
+      this._sections[name]=options.fn(this);
+      return null;
+    }
+  } 
+});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+//устанавливаем порт
+app.set('port', process.env.PORT || 3000);
+
+//подлючаем путь к  главной странице
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+// пользовательская страница 404
+app.use(function(req, res){
+  res.status(404);
+  res.render('404');
+});
+
+// пользовательская страница 500
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log( 'Express запущен на http://localhost:' + app.get('port'));
+});
